ba0c6a9bf27727d357ad836ab23cf90d0eb394cd
Maksum Nemchenko <maksnemch2004@gmail.com>
2026-03-23 13:30:59 +0200

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,29 @@
-import React from 'react';
 import './App.css';
+import React, { useEffect } from 'react';
 import { getNumbers } from './utils';
+import { useSearchParams } from 'react-router-dom';
 import { Pagination } from './components/Pagination';
 import { useState } from 'react';
 
 const items = getNumbers(1, 42).map(n => `Item ${n}`);
 
-function getPostNumbers(perPage: number, currentPage: number): number[] {
-  const startIndex = (currentPage - 1) * perPage;
-  const endIndex = Math.min(startIndex + (perPage - 1), items.length - 1);
-
-  return [startIndex, endIndex];
-}
-
 export const App: React.FC = () => {
-  const [perPage, setPerPage] = useState(5);
-  const [currentPage, setCurrentPage] = useState(1);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const pageFromUrl = Number(searchParams.get('page')) || 1;
+  const perPageFromUrl = Number(searchParams.get('perPage')) || 5;
+  const [perPage, setPerPage] = useState(perPageFromUrl);
+  const [currentPage, setCurrentPage] = useState(pageFromUrl);
 
-  const onPageCange = (page: number) => {
+  useEffect(() => {
+    setSearchParams({ page: String(currentPage), perPage: String(perPage) });
+  }, [currentPage, perPage, setSearchParams]);
+
+  const onPageChange = (page: number) => {
     setCurrentPage(page);
   };
 
-  const [start, end] = getPostNumbers(perPage, currentPage);
+  const start = (currentPage - 1) * perPage;
+  const end = Math.min(start + (perPage - 1), items.length - 1);
 
   return (
     <div className="container">
@@ -32,14 +34,14 @@ export const App: React.FC = () => {
       </p>
 
       <div className="form-group row">
-        <div className="col-3 col-sm-2 col-xl-1"> 
+        <div className="col-3 col-sm-2 col-xl-1">
           <select
             data-cy="perPageSelector"
             id="perPageSelector"
             className="form-control"
             value={perPage}
             onChange={event => {
-              setPerPage(parseInt(event.target.value));
+              setPerPage(Number(event.target.value));
               setCurrentPage(1);
             }}
           >
@@ -60,7 +62,7 @@ export const App: React.FC = () => {
         total={items.length}
         perPage={perPage}
         currentPage={currentPage}
-        onPageChange={onPageCange}
+        onPageChange={onPageChange}
       />
       <ul>
         {items.slice(start, end + 1).map(item => (
