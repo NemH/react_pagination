e9071de6fb06d83b88eeee98128f0bb646f949d2
Maksum Nemchenko <maksnemch2004@gmail.com>
2026-03-23 17:18:45 +0200

    Solution

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
-import React = require('react');
 import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root') as HTMLElement).render(
